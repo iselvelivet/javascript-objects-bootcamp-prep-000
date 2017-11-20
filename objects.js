c24aa6ecddaa388b4c702c543a7a5a5caa7c7993
Tim Sander <tomzander88@yahoo.com>
2017-11-20 13:21:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 let artistName = {artistName: ''}
 let songTitle = {songTitle: ''}
 function updatePlaylist (playlist, key, val) {
-  let playlist = Object.assign(artistName, songTitle)
+  var playlist = Object.assign(artistName, songTitle)
 }
