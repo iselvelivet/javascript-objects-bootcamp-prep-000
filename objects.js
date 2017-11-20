b8df7ee23f4df8b3ec3024caad1431cfdec6c354
Tim Sander <tomzander88@yahoo.com>
2017-11-20 13:23:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 let playlist = {artistName: '[songTitle]'}
 function updatePlaylist (playlist, key, val) {
-  let playlist = Object.assign(artistName, songTitle)
+  Object.assign(artistName, songTitle)
 }
