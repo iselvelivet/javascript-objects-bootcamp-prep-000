95f94910c80d2120ab87912130b5d50ee34d14ef
Tim Sander <tomzander88@yahoo.com>
2017-11-20 13:05:36 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist = {artistName: 'artistName', songTitle: 'songTitle'}
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist.artistName = artistName
+  playlist.songTitle = songTitle
+  return playlist
+}
